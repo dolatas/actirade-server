6193054da79ec2bba1d86de66f187fd1600703e5
dolatas <dodo.dolata@gmail.com>
2016-10-02 14:22:56 +0200

    build error fix

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -5,9 +5,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @ComponentScan
@@ -24,13 +21,3 @@ public class Application extends SpringBootServletInitializer {
     }
 
 }
-
-
-@RestController
-class GreetingController {
-
-    @RequestMapping("/hello/{name}")
-    String hello(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
-} 
